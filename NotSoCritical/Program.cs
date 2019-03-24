fb80b48083bae91ff614251a92362fd287bf238c
Yaroslav Kibysh <yanet.prod@gmail.com>
2019-03-24 14:24:36 +0200

    Add project files.

---
+++ NotSoCritical/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NotSoCritical
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
